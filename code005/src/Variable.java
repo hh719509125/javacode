e9e09e094a19b0eae08d9c4bbd7c3894313a94f0
hh719509125 <103106708+hh719509125@users.noreply.github.com>
2022-04-06 17:49:50 +0800

    Add files via upload

---
+++ code005/src/Variable.java
@@ -0,0 +1,18 @@
+public class Variable {
+    public static void main(String[] args){
+
+        int hh;
+        hh = 99999999;
+
+        int a=3;
+        int b=5;
+        int c=7;
+
+        int x=9;
+
+        int y=a*x + b*x*x + c*x*x*x;
+
+        System.out.println(y);
+        System.out.println(hh);
+    }
+}
